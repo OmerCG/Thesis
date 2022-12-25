47314cde42812dffa16c6a7b7135046ae8f7bd01
Nadav Geva <nadav@commonground-ai.com>
2022-12-25 21:32:37 +0200

    refactoring

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="clip2mesh",
+    version="0.0.1",
+    author="",
+    author_email="",
+    description="clip 2 shape repository",
+    package_data={"": ["license.txt"]},
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+)
